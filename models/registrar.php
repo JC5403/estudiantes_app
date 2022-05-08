03f14df021be980e06212e7b349331d337d1a146
Your Name <you@example.com>
2022-05-07 23:51:09 -0500

    registro

--- models/registrar.php
+++ models/registrar.php
@@ -1,5 +1,17 @@
 <?php
 
 require_once dirname(__DIR__) . '/db/conexion_db.php';
-require_once dirname(__DIR__) . '/db/conexion_db.php';
+require_once dirname(__DIR__) . '/controllers/i_controller.php';
 
+require_once dirname(__DIR__) . '/models/estudiantes.php';
+require_once dirname(__DIR__) . '/controllers/estudiantes_controller.php';
+
+
+use controllers\EstudianteController;
+use models\Estudiante;
+
+$estudianteController = new EstudianteController();
+
+$estudiante = new Estudiante ();
+
+$estudiante->set('codigo',$_POST['codigoinput']);
