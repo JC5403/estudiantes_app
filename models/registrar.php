e3e7b743d670386b1041a1836b0d51d4afb30624
Your Name <you@example.com>
2022-05-08 00:00:18 -0500

    registrar completo

--- models/registrar.php
+++ models/registrar.php
@@ -14,4 +14,34 @@ $estudianteController = new EstudianteController();
 
 $estudiante = new Estudiante ();
 
-$estudiante->set('codigo',$_POST['codigoinput']);
+$estudiante->set('codigo',$_POST['codigoInput']);
+$estudiante->set('nombres',$_POST['nombresInput']);
+$estudiante->set('apellidos',$_POST['apellidosInput']);
+$estudiante->set('edad',$_POST['edadInput']);
+
+$resultado = $estudianteController->create($estudiante);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro</title>
+</head>
+<body>
+    <h1>Resultado de la operación</h1>
+    <?php
+    if($resultado){
+        echo '<p>Registro exitoso</p>';
+        echo '<br>';
+        echo  '<a href="index.php">Volver a la lista </a>';
+    }else {
+        echo '<p>Se presento un error al guardar los datos, vuelva a intentarlo.</p>';
+        echo'<br>';
+        echo '<a href="from_estudiantes.php">Volver</a>';
+    }
+
+?>
+</body>
+</html>
